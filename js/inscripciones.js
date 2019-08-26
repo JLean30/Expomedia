02fcea3b54ab12763f09c2dcc1754f1206ed80d0
Jean Brenes <jlean30@protonmail.com>
2019-08-26 12:28:40 -0600

    cupos

---
+++ js/inscripciones.js
@@ -0,0 +1,231 @@
+const talleres = [{
+    id: '#1',
+    value: 'TallerintroductoriodeUnrealAndrésChinchilla-AulaN01',
+    name: 'Taller introductorio de Unreal Andrés Chinchilla - Aula N01',
+
+},
+{
+    id: '#2',
+    value: 'EsencialCostaRica,EconomíaNaranjaKevinVillalta-AulaN03',
+    name: 'Esencial Costa Rica, Economía NaranjaKevin Villalta - Aula N03',
+
+},
+{
+    id: '#3',
+    value: 'AsísomoslosticosyasípodemossermejoresGeovannyDebrusJiménez-AulaN09',
+    name: 'Así somos los ticos y así podemos ser mejores Geovanny "Debrus" Jiménez - Aula N09',
+
+},
+{
+    id: '#4',
+    value: 'TreeInteractive,experienciasenlaindustriadelvideojuegoFelipeCartín,AndrésCartín-AulaN02',
+    name: 'Tree Interactive, experiencias en la industria del videojuego Felipe Cartín, Andrés Cartín - Aula N02',
+
+},
+{
+    id: '#5',
+    value: 'ElejercicioprofesionaldelartistaplásticoRicardoVega-N04',
+    name: 'El ejercicio profesional del artista plástico Ricardo Vega - N04',
+
+},
+{
+    id: '#6',
+    value: 'Taller:ContenedoresdeDockerManuelEmilioDelgadoLópez-AulaN05',
+    name: 'Taller: Contenedores de Docker Manuel Emilio Delgado López - Aula N05',
+
+},
+{
+    id: '#7',
+    value: 'Torneo:HookbotsporTreeInteractive',
+    name: 'Torneo de Hookbots por Tree Interactive',
+
+},
+{
+    id: '#8',
+    value: 'Taller:DiseñodeaplicacionesAndrésChinchilla-AulaN01',
+    name: 'Taller: Diseño de aplicaciones Andrés Chinchilla -Aula N01',
+
+},
+{
+    id: '#9',
+    value: 'CibersecurityTrendsJosephChávezVelasco-AulaN02',
+    name: 'Cibersecurity Trends Joseph Chávez Velasco - Aula N02',
+
+},
+{
+    id: '#10',
+    value: 'DesarrollodepelículasanimadasenCRLeoRojas-AulaN03',
+    name: 'Desarrollo de películas animadas en CR Leo Rojas -Aula N03',
+
+},
+{
+    id: '#11',
+    value: 'MuestrayTaller:CanalesyherramientasdemercadeodigitalYohannaÁlvarezCordero-AulaN04',
+    name: 'Muestra y Taller: Canales y herramientas de mercadeo digital Yohanna Álvarez Cordero - Aula N04',
+
+},
+{
+    id: '#12',
+    value: 'TallerdelocuciónyactuaciónvocalCarlosBogarín-EstudiodeGrabación',
+    name: 'Taller de locución y actuación vocal Carlos Bogarín - Estudio de Grabación',
+
+},
+{
+    id: '#13',
+    value: 'Elfuturodelaproducción,mediosyentretenimiento:PelículasDidierRamírezDarjo-AulaN01',
+    name: 'El futuro de la producción, medios y entretenimiento: PelículasDidier Ramírez Darjo - Aula N01',
+
+},
+{
+    id: '#14',
+    value: 'Disenoprototipadoeimpresion3DenIMAulaN02',
+    name: 'Diseño, prototipado e impresión 3D en IM Luis Diego Hidalgo Calvo - Aula N02',
+
+},
+{
+    id: '#15',
+    value: 'DiseñodearteparavideojuegosSebastiánBrenesAndresChAulaN03',
+    name: 'Diseño de arte para videojuegos Sebastián Brenes, Andres Ch. - Aula N03',
+
+},
+{
+    id: '#16',
+    value: 'Taller:ExperienciadeUsuarioMarcosMolina-AulaN04',
+    name: 'aller: Experiencia de Usuario Marcos Molina -Aula N04',
+
+},
+{
+    id: '#17',
+    value: 'Taller:IntroducciónaUnityAndrésChinchilla-AulaN01',
+    name: 'Taller: Introducción a Unity Andrés Chinchilla - Aula N01',
+
+},
+{
+    id: '#18',
+    value: 'RealidadAumentadaITM,Licenciaturaenvideojuegos-AulaN02',
+    name: 'Realidad Aumentada ITM, Licenciatura en videojuegos - Aula N02',
+
+},
+{
+    id: '#19',
+    value: 'IngenieríaSocialyhackingBrandonCisneros-AulaN03',
+    name: 'Ingeniería Social y hacking Brandon Cisneros - Aula N03',
+
+},
+{
+    id: '#20',
+    value: 'Elfuturodecómohacerlascosas:Mediosyentretenimiento:VideojuegosDidierRamírezDarjo-AulaN04',
+    name: 'El futuro de cómo hacer las cosas: Medios y entretenimiento: Videojuegos Didier Ramírez Darjo - Aula N04',
+
+},
+{
+    id: '#21',
+    value: 'ElequipodeITdelfuturoJoséPabloEsquivelEscalante-AulaN02',
+    name: 'El equipo de IT del futuro José Pablo Esquivel Escalante - Aula N02',
+
+},
+{
+    id: '#22',
+    value: 'ElMétodoBarguetJulioJonathanSalazar-AulaN03',
+    name: 'El Método Barguet Julio Jonathan Salazar - Aula N03',
+
+},
+{
+    id: '#23',
+    value: 'Taller:ReactBásicoNikolGonzálezSequeira-AulaN05',
+    name: 'Taller: React Básico Nikol González Sequeira - Aula N05',
+
+},
+{
+    id: '#24',
+    value: 'TallerdefotografíacondispositivosmóvilesJhonPérezLópez-AulaN09',
+    name: 'Taller de fotografía con dispositivos móviles Jhon Pérez López - Aula N09',
+
+},
+{
+    id: '#25',
+    value: 'OportunidadesdeestudioenChinaCésarWang-AulaN05',
+    name: 'Oportunidades de estudio en China César Wang - Aula N05',
+
+},
+{
+    id: '#26',
+    value: 'RetodeprogramacionAulaN02',
+    name: 'Reto de programación Aula N02',
+
+},
+{
+    id: '#27',
+    value: 'RetodefotografiaAulaN03',
+    name: 'Reto de fotografía Aula N03',
+
+},
+{
+    id: '#28',
+    value: 'RetodedibujodigitalAulaN04',
+    name: 'Reto de dibujo digital Aula N04',
+
+},
+{
+    id: '#29',
+    value: 'RetodediseñodevideojuegosAulaN01',
+    name: 'Reto de diseño de videojuegos Aula N01',
+
+},
+];
+var firebaseConfig = {
+    apiKey: "AIzaSyABOvaG6mUhqCDFxJOyNMrzKoDg4-6ZSGs",
+    authDomain: "expomedia2019-828c9.firebaseapp.com",
+    databaseURL: "https://expomedia2019-828c9.firebaseio.com",
+    projectId: "expomedia2019-828c9",
+    storageBucket: "",
+    messagingSenderId: "521726918700",
+    appId: "1:521726918700:web:1f6fe9228ae1877e"
+};
+
+// Initialize Firebase
+let app = firebase.initializeApp(firebaseConfig);
+let db = app.database();
+$(document).ready(function () {
+    
+      
+      var saveData = (function () {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        return function (data, fileName) {
+            var json = JSON.stringify(data),
+                blob = new Blob([json], {type: "octet/stream"}),
+                url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        };
+    }());
+ 
+        fileName = "my-download.json";
+    
+    var data = [];
+    
+    var len = talleres.length;
+    for (var i = 0; i < len; i++) {
+        db.ref("inscripciones/" + talleres[i].value).once("value", snapshot => {
+        value= snapshot.val();
+        taller = snapshot.key;
+        if(value != null){
+            $('#cupos').append("<li class='list-group-item d-flex justify-content-between align-items-center'>"+taller+"<span class='badge badge-primary badge-pill'>Cupos: "+value.limite+"</span></li>");
+            
+        }
+            
+
+        });
+      
+            
+            
+        
+    }
+
+
+
+});
